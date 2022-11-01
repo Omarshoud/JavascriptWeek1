b32d2938e25287169bb2f5d4f99807453da8515b
Omar Marshoud <omarshoud@gmail.com>
2022-11-01 17:45:55 +0100

    airbnb configuration

---
+++ js/index.js
@@ -0,0 +1,12 @@
+import { foo } from './sample.js';
+
+foo();
+
+const omar = () => {
+    return 'hola bro';
+};
+console.log(omar());
+
+omar.apellido = 'marshoud';
+
+console.log(omar);
---
+++ js/sample.js
@@ -0,0 +1,6 @@
+export const foo = function () {
+    const x = 2;
+    const y = 4;
+    const r = x + y;
+    console.log('hola mundo', r);
+};
