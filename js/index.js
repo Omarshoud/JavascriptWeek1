e691b78f2496f837bd5b9b7d37d3128d57ee22c6
Omar Marshoud <omarshoud@gmail.com>
2022-11-02 21:37:28 +0100

    app

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-import { foo } from './sample.js';
+import strictEquals from './app';
 
-foo();
-
-const omar = () => {
-    return 'hola bro';
-};
-console.log(omar());
-
-omar.apellido = 'marshoud';
-
-console.log(omar);
+console.table([
+    { test: '1 & 1', result: strictEquals(1, 1) },
+    { test: 'NaN & NaN', result: strictEquals(NaN, NaN) },
+    { test: '0 & -0', result: strictEquals(0, -0) },
+    { test: '-0 & 0', result: strictEquals(-0, 0) },
+    { test: '1 & "1"', result: strictEquals(1, '1') },
+    { test: 'true & false', result: strictEquals(true, false) },
+    { test: 'false & false', result: strictEquals(false, false) },
+    { test: '"Water" & "oil"', result: strictEquals('Water', 'oil') },
+]);
--- js/sample.js
+++
@@ -1,6 +0,0 @@
-export const foo = function () {
-    const x = 2;
-    const y = 4;
-    const r = x + y;
-    console.log('hola mundo', r);
-};
